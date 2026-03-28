71ea954ac45c718c9d2fca56466c4b49b10efbc7
Jacob Lindey <jacoblindey@gmail.com>
2026-03-28 12:27:13 -0400

    feat: horizontal tile matching
    - refactored tile matching code to be more generic

--- games/match-3/puzzle/tiles/TileMatcher.cs
+++ games/match-3/puzzle/tiles/TileMatcher.cs
@@ -14,30 +14,66 @@ public partial class TileMatcher : Node
             {
                 continue;
             }
-            checkedCells.Add(cell);
-            
-            var hasUpNeighbor = grid.TryGetCell(cell.Coordinates + Vector2I.Up, out var upNeighbor);
-            var hasDownNeighbor = grid.TryGetCell(cell.Coordinates + Vector2I.Down, out var downNeighbor);
 
-            if (
-                hasUpNeighbor &&
-                hasDownNeighbor &&
-                cell.TryGetTile(out var tile) &&
-                upNeighbor.TryGetTile(out var upTile) &&
-                downNeighbor.TryGetTile(out var downTile) &&
-                tile.Type.Name == upTile.Type.Name &&
-                tile.Type.Name == downTile.Type.Name)
+            checkedCells.Add(cell);
+
+            if (TryVerticalMatch(grid, cell, out var match))
             {
-                GD.Print("!!found match!!");
-                GD.Print($"\t{upNeighbor.Coordinates} | {upTile.Type.Name}");
-                GD.Print($"\t{cell.Coordinates} | {cell.ContainedTile.Type.Name}");
-                GD.Print($"\t{downNeighbor.Coordinates} | {downTile.Type.Name}");
-                foundMatches.Add(new TileMatch([upNeighbor, cell, downNeighbor]));
+                foundMatches.Add(match);
+            }
+
+            if (TryHorizontalMatch(grid, cell, out match))
+            {
+                foundMatches.Add(match);
             }
         }
 
         matches = foundMatches.ToArray();
-        return matches.Length != 0;
+        return foundMatches.Count != 0;
+    }
+
+    private static bool TryVerticalMatch(Grid grid, GridCell cell, out TileMatch match)
+    {
+        return TryMatch(grid, cell, out match, Vector2I.Up, Vector2I.Down);
+    }
+
+    private static bool TryHorizontalMatch(Grid grid, GridCell cell, out TileMatch match)
+    {
+        return TryMatch(grid, cell, out match, Vector2I.Left, Vector2I.Right);
+    }
+
+    private static bool TryMatch(Grid grid, GridCell cell, out TileMatch match, params Vector2I[] relativeChecks)
+    {
+        match = null;
+        if (!cell.HasTile)
+        {
+            return false;
+        }
+
+        var centerTile = cell.ContainedTile;
+        List<GridCell> groupCells = [cell];
+        foreach (var otherPos in relativeChecks)
+        {
+            if (!grid.TryGetCell(cell.Coordinates + otherPos, out var otherCell))
+            {
+                return false;
+            }
+
+            if (!otherCell.TryGetTile(out var otherTile))
+            {
+                return false;
+            }
+
+            if (centerTile.Type.Name != otherTile.Type.Name)
+            {
+                return false;
+            }
+
+            groupCells.Add(otherCell);
+        }
+
+        match = new TileMatch(groupCells);
+        return true;
     }
 }
 
